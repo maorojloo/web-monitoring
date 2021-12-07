8b6f920a4c5478ca0eeb7d99768b6b867c06a0ce
ma.orojloo <ma.orojloo@gmail.com>
2021-12-07 07:44:18 +0330

    addin read me, addin env, addin req.txt and done:)

--- main.py
+++ main.py
@@ -1,51 +1,58 @@
-import os
 import smtplib
-import requests
-# import logging
-from linode_api4 import LinodeClient, Instance
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import schedule
+import time
+import urllib3
 
-EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
-EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-LINODE_TOKEN = os.environ.get('LINODE_TOKEN')
-
-# logging.basicConfig(filename='PATH_TO_DESIRED_LOG_FILE',
-#                     level=logging.INFO,
-#                     format='%(asctime)s:%(levelname)s:%(message)s')
-
-
-def notify_user():
-    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-        smtp.ehlo()
-        smtp.starttls()
-        smtp.ehlo()
-
-        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-
-        subject = 'YOUR SITE IS DOWN!'
-        body = 'Make sure the server restarted and it is back up'
-        msg = f'Subject: {subject}\n\n{body}'
-
-        # logging.info('Sending Email...')
-        smtp.sendmail(EMAIL_ADDRESS, 'INSERT_RECEIVER_ADDRESS', msg)
+#---------------------------------------------------------------
+sender_email_addr='web.alert.bot.ma@gmail.com'
+sender_email_pass='**********'
+receiver_email_addr='mohammadaminorojloo@outlook.com' 
+rechecktime=5 #secend
+emailmsg="THE WEBSITE IS FUCKEDUP"  #email msg
+emailtitle="WEB ALLERT [NO REPLY]"  #email title
+WEBSITE_url='http://127.0.0.1:8000' #http[s]://xxxxxxxxxx.xxx
+#-----------------------------------------------------------------
 
 
-def reboot_server():
-    client = LinodeClient(LINODE_TOKEN)
-    my_server = client.load(Instance, 376715)
-    my_server.reboot()
-    # logging.info('Attempting to reboot server...')
+def send_email(Subject,mail_content):
+    #The mail addresses and password
+    sender_address = sender_email_addr
+    sender_pass = sender_email_pass
+    receiver_address = receiver_email_addr
+    #Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = Subject   #The subject line
+    #The body and the attachments for the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+    #Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+    session.starttls() #enable security
+    session.login(sender_address, sender_pass) #login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print('Mail Sent')
 
-try:
-    r = requests.get('http://127.0.0.1:8000/', timeout=5)
-    if r.status_code != 200:
-        # logging.info('Website is DOWN!')
-        notify_user()    
-except:
-    pass
-
-
-
-
-
-
-   
+http = urllib3.PoolManager()
+print('the script is runn')
+def check_web ():
+    try:
+        r = http.request('GET',WEBSITE_url , TimeoutError=5)
+        print(r.status)
+        if r.status != 200:
+            print("web is down")
+            send_email(emailtitle,emailmsg)
+        else:
+            print("web is up")
+    except :
+        print("web is down")
+        send_email(emailtitle,emailmsg)
+     
+schedule.every(rechecktime).seconds.do(check_web)
+while True:
+    schedule.run_pending()
+    time.sleep(2)
